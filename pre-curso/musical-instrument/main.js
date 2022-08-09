98baf601b0af4468f286751dbc8bd91a4e9f7ab7
Pablo Lozano <juan.lozano@wizeline.com>
2022-08-08 23:52:37 -0500

    .

--- pre-curso/musical-instrument/main.js
+++ pre-curso/musical-instrument/main.js
@@ -1,1 +1,19 @@
 console.log('Hola mundo!');
+const myName = 'Pablo';
+let myAge = 22;
+let x = 3;
+let y = 4;
+const likesMusic = true;
+console.log('myName',myName);
+console.log('myAge',myAge);
+console.log('x',x);
+console.log('y',y);
+console.log('likesMusic',likesMusic);
+
+myName='Juan';
+
+console.log('myName',typeof myName);
+console.log('myAge',typeof myAge);
+console.log('x',typeof x);
+console.log('y',typeof y);
+console.log('likesMusic',typeof likesMusic);
